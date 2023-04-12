81830c8feb0a5b22df3214ee169041dce4b9735e
gonzalosinnott <gonzalo.sinnott@gmail.com>
2023-04-12 15:18:32 -0300

    12-03-23 - Se agrega el test eliminarElementoSimetricoTest

--- src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
+++ src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
@@ -7,28 +7,30 @@ import frgp.utn.edu.ar.main.MatrizAdyacencia;
 class MatrizAdyacenciaTest {
 	
 	private int cantidad = 4;
-	private MatrizAdyacencia matriz = new MatrizAdyacencia(Cantidad);
+	private MatrizAdyacencia matriz = new MatrizAdyacencia(cantidad);
 	
 	/// Punto A: agregarElementoTest
 	@Test
     public void agregarElementoTest() {
-        //MatrizAdyacencia matriz = new MatrizAdyacencia(3); 
+		/// Arrange 
         matriz.agregarElemento(1, 2); 
-        assertTrue(matriz.existeElemento(1, 2)); 
+		/// Act
+		boolean check = matriz.existeElemento(1, 2);
+		/// Assert
+		assertTrue(check);
     }
 	
 	/// Punto B: agregarElementoSimetriaTest
 	@Test
-	public void agregarElementoSimetriaTest() {
-		
+	public void agregarElementoSimetriaTest() {		
 		/// Arrange 
-		//MatrizAdyacencia matriz = new MatrizAdyacencia(4); 
 		matriz.agregarElemento(2, 3);
 		/// Act
 		boolean check = matriz.existeElemento(3, 2);
 		/// Assert
 		assertTrue(check);
 	}
+	
 	/// Punto C: eliminarElementoTest
 	@Test
 	public void eliminarElementoTest() {
@@ -42,6 +44,16 @@ class MatrizAdyacenciaTest {
 	}
 	
 	/// Punto D: eliminarElementoSimetricoTest
+	@Test
+	public void eliminarElementoSimetricoTest() {
+		/// Arrange
+		matriz.agregarElemento(2,3);
+		matriz.eliminarElemento(2,3);
+		/// Act
+		boolean check = matriz.existeElemento(3,2);
+		/// Assert
+		assertFalse(check);
+	}
 	
 	/// Punto E: contarRelacionesTest
 	
