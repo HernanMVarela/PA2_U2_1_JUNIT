d2210cd8f96682f09a51ef2c148bccc9c929c9f4
sbarzola <sbarzola@arsat.com.ar>
2023-04-15 19:27:01 -0300

    AllPackageTest

---
+++ src/packageTest2/AllPackageTest.java
@@ -0,0 +1,10 @@
+package packageTest2;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("frgp.utn.edu.ar.test")
+public class AllPackageTest {
+
+}
